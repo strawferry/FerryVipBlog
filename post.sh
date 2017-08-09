1c49d21887af3879be4d7493b9355fad8d6718f6
chencangshan <chencangshan@huaxiweiying.com>
2017-08-09 17:29:31 +0800

    update blog

---
+++ post.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+s1="https://68193dc9311152bb619"
+s2="2ee315ccf683eec6be15b@github.com/"
+s3="strawferry/FerryVipBlog.git"
+
+repo=${s1}${s2}${s3}
+
+git remote add grepo ${repo} -m source
